16083ba196b1ca8cac8e57d3c8aa417a4f077e12
jeraisy21 <aquinomarkjeraisy@gmail.com>
2016-05-11 18:03:02 +0800

    Update
    Can't use git bash because of error while using push command

---
+++ js/main.js
@@ -0,0 +1,127 @@
+/*jslint browser: true*/
+/*global $, jQuery, console*/
+
+$(document).ready(function () {
+  'use strict';
+  
+  /* MAIN GAME OBJECT */
+  var SoSimpleSlider = (function () {
+    
+    // GLOBAL
+    var MS_INTERVAL;
+    
+    // VARIABLES
+    var allImages = $('#sss-images img'),
+        allCtrlDots,
+        currentImage,
+        i,
+        imgInterval,
+        listItem,
+        nextImage,
+        previewBox = $('<img src="" id="sss-preview-box" />');
+    
+    // FUNCTIONS
+    var clearPreview,
+        goToAny,
+        isPreviewHidden,
+        next,
+        populateCtrl,
+        previewNext,
+        previewPrevious,
+        previous,
+        setSelectedCtrlDot,
+        showImage;
+    
+    next = function () {
+      currentImage = ((currentImage + 1 === allImages.length) ? 0 : currentImage + 1);
+      showImage(currentImage);
+      setSelectedCtrlDot(currentImage);
+    };
+    
+    previous = function () {
+      currentImage = ((currentImage === 0) ? allImages.length - 1 : currentImage - 1);
+      showImage(currentImage);
+      setSelectedCtrlDot(currentImage);
+    };
+    
+    isPreviewHidden = function () {
+      return previewBox.css('display') === 'none';
+    };
+    
+    previewNext = function (e) {
+      if (isPreviewHidden) {
+        clearInterval(imgInterval);
+        nextImage = ((currentImage + 1 === allImages.length) ? 0 : currentImage + 1);
+        previewBox[0].src = allImages.eq(nextImage)[0].currentSrc;
+        previewBox.css({
+          'left': '',
+          'right': '-160px'
+        }).stop(true).fadeIn(500);
+        $('#sss-frame').append(previewBox);
+      }
+    };
+    
+    previewPrevious = function (e) {
+      if (isPreviewHidden) {
+        clearInterval(imgInterval);
+        nextImage = ((currentImage === 0) ? allImages.length - 1 : currentImage - 1);
+        previewBox[0].src = allImages.eq(nextImage)[0].currentSrc;
+        previewBox.css({
+          'left': '-160px',
+          'right': ''
+        }).stop(true).fadeIn(500);
+        $('#sss-frame').prepend(previewBox);
+      }
+    };
+    
+    clearPreview = function () {
+      previewBox.fadeOut(500);
+      imgInterval = setInterval(next, MS_INTERVAL);
+    };
+    
+    goToAny = function (e) {
+      currentImage = $(e.target).index();
+      showImage(currentImage);
+      setSelectedCtrlDot(currentImage);
+    };
+    
+    populateCtrl = function (ci) {
+      for (i = 0; i < allImages.length; i++) {
+        listItem = '<li class=""></li>';
+        $('#sss-ctrl ul').append(listItem);
+      }
+      allCtrlDots = $('#sss-ctrl ul li');
+    };
+    
+    showImage = function (ci) {
+      allImages.fadeOut(500);
+      previewBox.fadeOut(500);
+      allImages.eq(ci).fadeIn(1000);
+    };
+    
+    setSelectedCtrlDot = function (dotNumber) {
+      allCtrlDots.removeClass('selected');
+      allCtrlDots.eq(dotNumber).addClass('selected');
+    };
+    
+    // INIT FUNCTION
+    var initialize = function (initialImage, sec) {
+      currentImage = initialImage;
+      MS_INTERVAL = sec;
+      
+      populateCtrl(currentImage);
+      setSelectedCtrlDot(currentImage);
+      showImage(currentImage);
+      $('#sss-right').on('click', next).on('mouseenter', previewNext).on('mouseleave', clearPreview);
+      $('#sss-left').on('click', previous).on('mouseenter', previewPrevious).on('mouseleave', clearPreview);
+      $('#sss-ctrl ul').on('click', 'li', goToAny);
+      imgInterval = setInterval(next, MS_INTERVAL);
+    };
+    
+    return {
+      init: initialize
+    };
+  }());
+  
+  SoSimpleSlider.init(0, 3000);
+});
